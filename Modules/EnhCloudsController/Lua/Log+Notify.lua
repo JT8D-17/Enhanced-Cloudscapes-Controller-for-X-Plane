b9b8115c7e9409343525d90e69640e78ecb88989
JT8D-17 <amusing.random.alias@gmail.com>
2020-08-15 05:54:10 +0200

    Add missing files

---
+++ Modules/EnhCloudsController/Lua/Log+Notify.lua
@@ -0,0 +1,74 @@
+--[[
+
+Lua Module, required by EnhCloudsController.lua
+Licensed under the EUPL v1.2: https://eupl.eu/
+
+]]
+--[[
+
+VARIABLES (local to this module)
+
+]]
+local ECC_LogFile = MODULES_DIRECTORY.."EnhCloudsController/Log.txt"          -- Log file path
+ECC_NotificationStack = { }                     -- Array for the notification stack 
+local toremove = {}
+--[[
+
+FUNCTIONS
+
+]]
+--[[ Write to log file ]]
+function ECC_Log_Write(string)
+	local file = io.open(ECC_LogFile, "a") -- Check if file exists
+	file:write(os.date("%x, %H:%M:%S"),": ",string,"\n")
+	file:close()
+end
+--[[ Delete log file ]]
+function ECC_Log_Delete()
+    os.remove(ECC_LogFile) ECC_Notification("FILE DELETE: "..ECC_LogFile,"Warning","log")
+end
+--[[ Available types: "Success","Advisory","Warning","Error"]]
+function ECC_Notification(messagestring,messagetype,writelog)
+    ECC_NotificationStack[(#ECC_NotificationStack+1)] = {}
+    ECC_NotificationStack[#ECC_NotificationStack][1] = messagestring
+    ECC_NotificationStack[#ECC_NotificationStack][2] = messagetype
+    ECC_NotificationStack[#ECC_NotificationStack][3] = os.clock() + ECC_SettingsValGet("NotificationDispTime")
+    --print(table.concat(ECC_NotificationStack[#ECC_NotificationStack],";",1,#ECC_NotificationStack[#ECC_NotificationStack]))
+    if writelog == "log" then
+        ECC_Log_Write(messagestring)
+    end
+end
+--[[ 
+
+IMGUI WINDOW ELEMENT
+
+]]
+function ECC_Win_Notifications()
+    --------------------------------------------------------
+	imgui.Separator()
+	--------------------------------------------------------
+    imgui.TextUnformatted("Notifications:")
+    -- Only display when message stack table is empty
+    if #ECC_NotificationStack == 0 then
+        imgui.TextUnformatted("(None)")
+    end
+    -- Loop through stack, see if a stack message is valid, then display it colored according to type. If not valid anymore, mark it for deletion by noting the subtable index
+    for k=1,#ECC_NotificationStack do
+        if os.clock() <= ECC_NotificationStack[k][3] then
+            if tostring(ECC_NotificationStack[k][2]) == "Success" then imgui.PushStyleColor(imgui.constant.Col.Text, ECC_ImguiColors[5]) imgui.TextUnformatted(ECC_NotificationStack[k][1]) imgui.PopStyleColor()
+            elseif tostring(ECC_NotificationStack[k][2]) == "Advisory" then imgui.PushStyleColor(imgui.constant.Col.Text, ECC_ImguiColors[6]) imgui.TextUnformatted(ECC_NotificationStack[k][1]) imgui.PopStyleColor() 
+            elseif tostring(ECC_NotificationStack[k][2]) == "Warning" then imgui.PushStyleColor(imgui.constant.Col.Text, ECC_ImguiColors[7]) imgui.TextUnformatted(ECC_NotificationStack[k][1]) imgui.PopStyleColor()
+            elseif tostring(ECC_NotificationStack[k][2]) == "Error" then imgui.PushStyleColor(imgui.constant.Col.Text, ECC_ImguiColors[4]) imgui.TextUnformatted(ECC_NotificationStack[k][1]) imgui.PopStyleColor() end
+        else
+            toremove[#toremove+1] = k
+        end
+    end
+    -- If index table for deletion is not empty, loop through it and delete the subtable from the message stack table by index
+    if #toremove > 0 then
+        for l=1,#toremove do
+            table.remove(ECC_NotificationStack,toremove[l])
+            --imgui.TextUnformatted(toremove[l])
+            toremove = {}
+        end
+    end
+end
---
+++ Modules/EnhCloudsController/Lua/Settings.lua
@@ -0,0 +1,209 @@
+--[[
+
+Lua Module, required by EnhCloudsController.lua
+Licensed under the EUPL v1.2: https://eupl.eu/
+
+]]
+--[[
+
+VARIABLES (global and local!)
+
+]]
+local ECC_SettingsFile = MODULES_DIRECTORY.."EnhCloudsController/UI_Prefs.cfg"   -- Preferences file path
+
+ECC_Settings = {
+    {"AutoSave",0},             -- Autosave enabled
+    {"AutoLoad",0},             -- Autoload enabled
+    {"WindowToggleByHotkey",0}, -- Window open/close by hotkey enabled
+    {"WindowToggleHotkey",85},  -- Window open/close hotkey (default: 85 = u)
+    {"WindowIsOpen",1},         -- Window open/close status
+    {"Window_X",100},           -- Window X position
+    {"Window_Y",400},           -- Window Y position
+    {"Window_W",530},           -- Window width
+    {"Window_H",600},           -- Window height
+    {"Window_Page",0},          -- Window page
+    {"NotificationDispTime",6}, -- Notification display time
+}
+--[[
+
+FUNCTIONS
+
+]]
+--[[ Accessor: Get name of a setting ]]
+function ECC_SettingsNameGet(item)
+    for i=1,#ECC_Settings do
+       if ECC_Settings[i][1] == item then return ECC_Settings[i][1] end
+    end
+end
+--[[ Accessor: Get value of a setting ]]
+function ECC_SettingsValGet(item)
+    for i=1,#ECC_Settings do
+       if ECC_Settings[i][1] == item then return ECC_Settings[i][2] end
+    end
+end
+--[[ Accessor: Set value of a setting ]]
+function ECC_SettingsValSet(item,newvalue)
+    for i=1,#ECC_Settings do
+       if ECC_Settings[i][1] == item then ECC_Settings[i][2] = newvalue break end
+    end
+end
+--[[ Update window position information ]]
+function ECC_GetWindowInfo()
+		if ECC_SettingsValGet("Window_W") ~= imgui.GetWindowWidth() or ECC_SettingsValGet("Window_H") ~= imgui.GetWindowHeight() or ECC_SettingsValGet("Window_X") ~= ECC_Window_Pos[1] or ECC_SettingsValGet("Window_Y") ~= ECC_Window_Pos[2] then
+			ECC_SettingsValSet("Window_W",imgui.GetWindowWidth())
+			ECC_SettingsValSet("Window_H",imgui.GetWindowHeight())
+			ECC_SettingsValSet("Window_X",ECC_Window_Pos[1])
+			ECC_SettingsValSet("Window_Y",ECC_Window_Pos[2])
+            --print(ECC_SettingsValGet("Window_X")..","..ECC_SettingsValGet("Window_Y")..","..ECC_SettingsValGet("Window_W")..","..ECC_SettingsValGet("Window_H"))
+			--ECC_Settings_CheckAutosave("NoLog")
+		end
+end
+--[[ Settings file write ]]
+function ECC_SettingsFileWrite(log)
+    ECC_Log_Write("FILE INIT WRITE: "..ECC_SettingsFile)
+    local file = io.open(ECC_SettingsFile, "w")
+    file:write(ECC_ScriptName.." settings file created/updated on ",os.date("%x, %H:%M:%S"),"\n\n")
+    for a=1,#ECC_Settings do
+        --print(ECC_Settings[a][1].."="..ECC_Settings[a][2])
+        file:write(ECC_Settings[a][1].."="..ECC_Settings[a][2].."\n")
+    end
+    if file:seek("end") > 0 then 
+        if log == "log" then ECC_Notification("FILE WRITE SUCCESS: "..ECC_SettingsFile,"Success","log") else ECC_Notification("FILE WRITE SUCCESS: "..ECC_SettingsFile,"Success") end
+    else 
+        if log == "log" then ECC_Notification("FILE WRITE ERROR: "..ECC_SettingsFile,"Error","log") else ECC_Notification("FILE WRITE ERROR: "..ECC_SettingsFile,"Error") end 
+    end
+	file:close()
+end
+--[[ Settings file read ]]
+function ECC_SettingsFileRead()
+    local file = io.open(ECC_SettingsFile, "r")
+    if file then
+        ECC_Log_Write("FILE INIT READ: "..ECC_SettingsFile)
+        local i = 0
+        local temptable = { }
+        for line in file:lines() do
+            if string.match(line,"^AutoLoad") then
+               ECC_Settings_LineSplit(line,"([^=]+)")
+               if tonumber(ECC_Settings_LineSplitResult[2]) == 0 then 
+                   --print("Aborting!")
+                   break
+               end
+            end
+            if string.match(line,"^[A-Z,a-z].+=") then
+                ECC_Settings_LineSplit(line,"([^=]+)")
+                temptable[#temptable+1] = ECC_Settings_LineSplitResult
+                --print(#temptable..": "..table.concat(temptable[#temptable],","))
+                i = i+1
+            end
+            for j=1,#temptable do
+                for k=1,#ECC_Settings do   
+                    if temptable[j][1] == ECC_Settings[k][1] then 
+                        --print("Match temptable "..temptable[j][1].." with Settings table "..ECC_Settings[k][1].." at "..k)
+                        ECC_Settings[k][2] = tonumber(temptable[j][2]) -- Current value(s)
+                    end
+                end
+            end       
+        end
+        file:close()
+        for l=1,#ECC_Settings do print(table.concat(ECC_Settings[l],": ")) end
+		if i ~= nil and i > 0 then ECC_Notification("FILE READ SUCCESS: "..ECC_SettingsFile,"Success","log") else ECC_Notification("FILE READ ERROR: "..ECC_SettingsFile,"Error","log") end
+    else
+        ECC_Notification("FILE NOT FOUND: "..ECC_SettingsFile,"Error","log")
+		--ECC_Check_AutoLoad = false
+	end   
+end
+--[[ Settings file delete ]]
+function ECC_SettingsFileDelete()
+   os.remove(ECC_SettingsFile) ECC_Notification("FILE DELETE: "..ECC_SettingsFile,"Warning") 
+end
+--[[ Check Autosave status ]]
+function ECC_Settings_CheckAutosave(log)
+    if ECC_SettingsValGet("AutoSave") == 1 then
+        if log == "log" then ECC_SettingsFileWrite("log")
+        else ECC_SettingsFileWrite() end
+    end
+end
+--[[ Determine string from value ]]
+function ECC_ValToStr(input)
+    local string = ""
+        if input == 0 then string = "Enable" 
+        elseif input == 1 then string = "Disable" end
+    return string
+end
+--[[ Splits a line at the designated delimiter ]]
+function ECC_Settings_LineSplit(input,delim)
+	ECC_Settings_LineSplitResult = {}
+	--print(input)
+	for i in string.gmatch(input,delim) do table.insert(ECC_Settings_LineSplitResult,i) end
+	--print("ECC_Settings_LineSplitResult: "..table.concat(ECC_Settings_LineSplitResult,",",1,#ECC_Settings_LineSplitResult))
+	return ECC_Settings_LineSplitResult
+end
+--[[
+
+IMGUI WINDOW ELEMENT
+
+]]
+function ECC_Win_Settings()
+    --[[ Button ]]
+    if ECC_SettingsValGet("Window_Page") == 0 then
+        imgui.Dummy((ECC_SettingsValGet("Window_W")-30),19)
+        if imgui.Button("ECC Preferences",(ECC_SettingsValGet("Window_W")-30),20) then ECC_SettingsValSet("Window_Page",1) ECC_Settings_CheckAutosave() end
+    end
+    --[[ Page ]]
+    if ECC_SettingsValGet("Window_Page") == 1 then    
+        --[[ "Back" button ]]
+        if imgui.Button("Main Menu",(ECC_SettingsValGet("Window_W")-30),20) then ECC_SettingsValSet("Window_Page",0) ECC_Settings_CheckAutosave() end
+        imgui.Dummy((ECC_SettingsValGet("Window_W")-30),10)
+        imgui.Separator()
+        --[[ Message display time ]]
+        imgui.PushItemWidth(((ECC_SettingsValGet("Window_W") / 2)-30))
+        local changed,buffer = imgui.InputInt("  Notification Display Time (Seconds) ##10",ECC_SettingsValGet("NotificationDispTime"),0,0)
+        if changed then ECC_SettingsValSet("NotificationDispTime",buffer) ECC_Settings_CheckAutosave() buffer = nil end
+        imgui.PopItemWidth()
+        imgui.Dummy((ECC_SettingsValGet("Window_W")-30),19)
+        --[[ Hotkey options ]]
+        if imgui.Button(ECC_ValToStr(ECC_SettingsValGet("WindowToggleByHotkey")).." Window Toggling By Hotkey ##20",(ECC_SettingsValGet("Window_W")-30),20) then 
+            if ECC_SettingsValGet("WindowToggleByHotkey") == 0 then ECC_SettingsValSet("WindowToggleByHotkey",1)
+            elseif ECC_SettingsValGet("WindowToggleByHotkey") == 1 then ECC_SettingsValSet("WindowToggleByHotkey",0) end 
+            ECC_Settings_CheckAutosave()
+        end
+        if ECC_SettingsValGet("WindowToggleByHotkey") == 1 then 
+            imgui.PushItemWidth(((ECC_SettingsValGet("Window_W") / 2)-30))
+            local changed,buffer = imgui.InputInt("  Keyboard Key Code##21",ECC_SettingsValGet("WindowToggleHotkey"),0,0) 
+            if changed then ECC_SettingsValSet("WindowToggleHotkey",buffer) ECC_Settings_CheckAutosave() buffer = nil end
+            imgui.PopItemWidth() 
+            imgui.PushStyleColor(imgui.constant.Col.Text, ECC_ImguiColors[7]) imgui.TextUnformatted("Find keyboard key codes with FlyWithLua's 'Show Keystroke Numbers' function (see FWL menu).\nAlternatively, you can assign a key to this script's 'Toggle Window' command in XP11's keyboard assignment menu. Make sure to disable this internal hotkey before doing so.") imgui.PopStyleColor()
+            imgui.Dummy((ECC_SettingsValGet("Window_W")-30),19)
+        end
+        --[[ Autosave options ]]
+        if imgui.Button(ECC_ValToStr(ECC_SettingsValGet("AutoSave")).." Autosave Changes##30",(ECC_SettingsValGet("Window_W")-30),20) then 
+            if ECC_SettingsValGet("AutoSave") == 0 then ECC_SettingsValSet("AutoSave",1)
+            elseif ECC_SettingsValGet("AutoSave") == 1 then ECC_SettingsValSet("AutoSave",0) end 
+            ECC_SettingsFileWrite()
+        end
+    --			if ECC_SettingsValGet("AutoSave") then
+    --				imgui.SameLine() imgui.TextUnformatted("(interval in seconds):") imgui.SameLine()
+    --				local changed, newAutoSave_Time = imgui.InputText("##41", ECC_Preferences.AutoSave_Time, 4)
+    --				if changed and newAutoSave_Time ~= "" and tonumber(newAutoSave_Time) then ECC_Preferences.AutoSave_Time = newAutoSave_Time ECC_SettingsFileWrite() end
+    --			end
+        --[[ Autoload options ]]
+        if imgui.Button(ECC_ValToStr(ECC_SettingsValGet("AutoLoad")).." Autoload Changes##40",(ECC_SettingsValGet("Window_W")-30),20) then 
+            if ECC_SettingsValGet("AutoLoad") == 0 then ECC_SettingsValSet("AutoLoad",1)
+            elseif ECC_SettingsValGet("AutoLoad") == 1 then ECC_SettingsValSet("AutoLoad",0) end 
+            ECC_SettingsFileWrite()
+        end       
+        imgui.Dummy((ECC_SettingsValGet("Window_W")-30),19)
+        -- [[ Window information ]]
+        imgui.TextUnformatted("Current Window Size (W,H) / Pos. (X,Y): "..imgui.GetWindowWidth()..","..imgui.GetWindowHeight().." / "..ECC_Window_Pos[1]..","..ECC_Window_Pos[2])
+        imgui.TextUnformatted("Screen Width / Height:                  "..SCREEN_WIDTH.." x "..SCREEN_HIGHT.." Px")
+        ECC_GetWindowInfo()
+        imgui.TextUnformatted("Window size is saved automatically!")
+        imgui.Dummy((ECC_SettingsValGet("Window_W")-30),19)
+        -- [[ Settings file control buttons ]]
+        if imgui.Button("Save UI Settings ##50",(ECC_SettingsValGet("Window_W")-30),20) then ECC_SettingsFileWrite() end
+        if imgui.Button("Load UI Settings ##60",(ECC_SettingsValGet("Window_W")-30),20) then ECC_SettingsFileRead() end
+        imgui.Dummy((ECC_SettingsValGet("Window_W")-30),19)
+        if imgui.Button("Delete UI Settings",(ECC_SettingsValGet("Window_W")-30),20) then ECC_SettingsFileDelete() ECC_Initialized = false end
+        -- End of settings page 		
+    end
+end
