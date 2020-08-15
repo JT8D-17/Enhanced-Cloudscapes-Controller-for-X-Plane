621cc53511b3160fc6bf036f1a4ffc1de15798ca
JT8D-17 <amusing.random.alias@gmail.com>
2020-08-15 05:50:07 +0200

    Finished backend rework of main window

--- Scripts/EnhCloudsController.lua
+++ Scripts/EnhCloudsController.lua
@@ -5,7 +5,6 @@ Licensed under the EUPL v1.2: https://eupl.eu/
 
 BK, xxyyzzzz
  ]]
- 
 --[[
 
 REQUIREMENTS
@@ -16,75 +15,58 @@ if not SUPPORTS_FLOATING_WINDOWS then
 	return
 end
 --[[ Required modules,DO NOT MODIFY LOAD ORDER! ]]
-require("EnhCloudsController/Lua/ECC_FileIO")           -- File input/output scripts
-require("EnhCloudsController/Lua/ECC_Notifications")    -- Imgui Window Element: Notifications
-require("EnhCloudsController/Lua/ECC_Globals")          -- Global variables and datarefs
-require("EnhCloudsController/Lua/ECC_Helpers")          -- Helper functions
-require("EnhCloudsController/Lua/ECC_Settings")         -- Imgui Window Element: Settings
-require("EnhCloudsController/Lua/ECC_CloudPrefs")       -- Cloud preferences window
-
+require("EnhCloudsController/Lua/Log+Notify")       -- Imgui Window Element: Notifications
+require("EnhCloudsController/Lua/Settings")         -- Imgui Window Element: Settings
+require("EnhCloudsController/Lua/CloudPrefs")       -- Imgui Window Element: Cloud preferences
 --[[
 
-PATHS
-
-]]
---ECC_AircraftSaveFile = AIRCRAFT_PATH.."Z_ECC_Config.cfg" 		     -- Aircraft save file path
-ECC_PrefsFile = MODULES_DIRECTORY.."EnhCloudsController/UI_Prefs.cfg"   -- Preferences file path
-ECC_LogFile = MODULES_DIRECTORY.."EnhCloudsController/Log.txt"          -- Log file path
---[[
-
-VARIABLES (local or global!)
+VARIABLES (local or global)
 
 ]]
 ECC_ScriptName = "Enhanced Cloudscapes Controller"
-local ECC_Initialized = false 	-- Has the script been initialized?
-ECC_Check_Autoload = false						-- Enables check of the autoloading condition
-ECC_Window_Pos={0,0}							-- Window position x,y
-ECC_NotificationStack = { }                     -- Array for the notification stack 
+local ECC_Initialized = false   -- Has the script been initialized?
+ECC_Check_Autoload = false      -- Enables check of the autoloading condition
+ECC_Window_Pos={0,0}            -- Window position x,y
 ECC_ImguiColors={0x33FFAE00,0xBBFFAE00,0xFFC8C8C8,0xFF0000FF,0xFF19CF17,0xFFB6CDBA,0xFF40aee5} -- Imgui: Control elements passive, control elements active, text, negative, positive, neutral, caution
 --[[
 
-IMGUI WINDOW MANAGEMENT
+FUNCTIONS
 
 ]]
 --[[ Show Window ]]
 function ECC_Window_Show()
-	ECC_Window = float_wnd_create(ECC_Preferences.AAA_Window_W, ECC_Preferences.AAA_Window_H, 1, true)
-	float_wnd_set_position(ECC_Window, ECC_Preferences.AAA_Window_X, ECC_Preferences.AAA_Window_Y)
+	ECC_Window = float_wnd_create(ECC_SettingsValGet("Window_W"), ECC_SettingsValGet("Window_H"), 1, true)
+	float_wnd_set_position(ECC_Window, ECC_SettingsValGet("Window_X"), ECC_SettingsValGet("Window_Y"))
 	float_wnd_set_title(ECC_Window, ECC_ScriptName)
 	float_wnd_set_imgui_builder(ECC_Window, "ECC_Window_Build")
 	float_wnd_set_onclose(ECC_Window, "ECC_Window_Hide")
-	ECC_Preferences.AAA_Is_ECC_Window_Open = true
-	ECC_CheckAutosave()
-	--print("Window open: "..tostring(ECC_Preferences.AAA_Is_ECC_Window_Open))
+	ECC_SettingsValSet("WindowIsOpen",1)
+	ECC_Settings_CheckAutosave()
+	--print("Window open: "..ECC_SettingsValGet("WindowIsOpen"))
 	ECC_Log_Write("Window Opening")
 end
 --[[ Hide Window ]]
 function ECC_Window_Hide(ECC_Window)
 	if ECC_Window then float_wnd_destroy(ECC_Window) end
-	ECC_Preferences.AAA_Is_ECC_Window_Open = false
-	ECC_CheckAutosave()
-	--print("Window open: "..tostring(ECC_Preferences.AAA_Is_ECC_Window_Open))
+	ECC_SettingsValSet("WindowIsOpen",0)
+	ECC_Settings_CheckAutosave()
+	--print("Window open: "..ECC_SettingsValGet("WindowIsOpen"))
 	ECC_Log_Write("Window Closing")
 end
 --[[ Toggle Window ]]
 function ECC_Window_Toggle()
-	if not ECC_Preferences.AAA_Is_ECC_Window_Open then ECC_Window_Show() else ECC_Window_Hide(ECC_Window) end
+	if ECC_SettingsValGet("WindowIsOpen") == 0  then ECC_Window_Show() else ECC_Window_Hide(ECC_Window) end
 end
 --[[ Open Window by Keystroke ]]
 function ECC_Window_By_Key()
-	if ECC_Settings.WindowToggleByHotkey == 1 and KEY_ACTION=="pressed" and VKEY==ECC_Preferences.AAA_WindowToggleHotkey then
+	if ECC_SettingsValGet("WindowToggleByHotkey") == 1 and KEY_ACTION=="pressed" and VKEY==ECC_SettingsValGet("WindowToggleHotkey") then
 		ECC_Window_Toggle()
 		RESUME_KEY = true
-		--print("Pressed "..ECC_Preferences.AAA_WindowToggleHotkey)
+		--print("Pressed "..ECC_SettingsValGet("WindowToggleHotkey"))
 	end
 end
 do_on_keystroke("ECC_Window_By_Key()")
---[[
-
-IMGUI WINDOW BUILDER
-
-]]
+--[[ Imgui window builder ]]
 function ECC_Window_Build(ECC_Window,xpos,ypos)
 	ECC_Window_Pos={xpos,ypos}
 	--[[ Window styling ]]
@@ -100,12 +82,12 @@ function ECC_Window_Build(ECC_Window,xpos,ypos)
 	imgui.PushStyleColor(imgui.constant.Col.HeaderActive,ECC_ImguiColors[2])
 	imgui.PushStyleColor(imgui.constant.Col.HeaderHovered,ECC_ImguiColors[2])
 	imgui.PushStyleColor(imgui.constant.Col.CheckMark,ECC_ImguiColors[3])
-    imgui.PushTextWrapPos(ECC_Preferences.AAA_Window_W-15)
-    imgui.Dummy((ECC_Preferences.AAA_Window_W-15),10)
+    imgui.PushTextWrapPos(ECC_SettingsValGet("Window_W")-30)
+    imgui.Dummy((ECC_SettingsValGet("Window_W")-30),10)
 	--[[ Window Content ]]
     ECC_Win_CloudPrefs()
 	ECC_Win_Settings()
-    imgui.Dummy((ECC_Preferences.AAA_Window_W-15),10)
+    imgui.Dummy((ECC_SettingsValGet("Window_W")-30),10)
     ECC_Win_Notifications()
 	--[[ End Window Styling ]]
 	imgui.PopStyleColor(12)
@@ -117,14 +99,19 @@ end
 INITIALIZATION
 
 ]]
-ECC_File_Delete("Log")					-- Delete the old log file
-ECC_Log_Write("INIT: Beginning "..ECC_ScriptName.." initialization")
-ECC_Table_Sort(ECC_Preferences)	 	-- Trigger sorting and recreation of initial preferences parameter table
-ECC_Check_Autoload = true 				--Enable autoloading status mode check
-ECC_File_Read("PrefsFile") 				--Trigger reading the save file and writing the contents to the target table
-if ECC_Preferences.AAA_Is_ECC_Window_Open then ECC_Window_Show() end -- If window open flag was true, build the window
-ECC_Initialized = true
-if ECC_Initialized then print("---> "..ECC_ScriptName.." initialized.") ECC_Log_Write("INIT: Finished "..ECC_ScriptName.." initialization") end
+--[[ Has to run in a 1 second loop to work ]]
+function ECC_Main_1sec()
+	if not ECC_Initialized then 
+        ECC_Log_Delete()					-- Delete the old log file
+        ECC_Log_Write("INIT: Beginning "..ECC_ScriptName.." initialization")
+        --ECC_Check_Autoload = true 				--Enable autoloading status mode check
+        ECC_SettingsFileRead() 				--Trigger reading the save file and writing the contents to the target table
+        if ECC_SettingsValGet("WindowIsOpen") == 1 then ECC_Window_Show() end -- If window open flag was true, build the window
+        ECC_Initialized = true
+        if ECC_Initialized then print("---> "..ECC_ScriptName.." initialized.") ECC_Log_Write("INIT: Finished "..ECC_ScriptName.." initialization") end
+    end
+end
+do_often("ECC_Main_1sec()")
 --[[
 
 MACROS AND COMMANDS
